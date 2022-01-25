34cbb8a36e0879b3323524103c409f29e34c1b2c
Vanity <ktkyletran@gmail.com>
2022-01-24 19:17:22 -0600

    added home container with mobile responsive navbar skeleton and set/get user from localstorage

---
+++ client/src/containers/Home/index.js
@@ -0,0 +1,64 @@
+import React, { useState, useEffect, useRef } from 'react';
+import { HiMenu } from 'react-icons/hi';
+import { AiFillCloseCircle } from 'react-icons/ai';
+import { Link, Route, Routes } from 'react-router-dom';
+import { Sidebar, UserProfile } from '../../components';
+import { client } from '../../client';
+import Pins from '../Pins';
+import { userQuery } from '../../utils/data';
+
+const Home = () => {
+  const scrollRef = useRef(null);
+  const [sidebar, setSidebar] = useState(false);
+  const [user, setUser] = useState(null);
+  const userInfo = localStorage.getItem('user') !== 'undefined' ? JSON.parse(localStorage.getItem('user')) : localStorage.clear();
+  
+  useEffect(() => {
+    const query = userQuery(userInfo?.googleId);
+
+    client.fetch(query)
+    .then((data) => {
+      setUser(data[0]);
+    });
+  }, [userInfo?.googleId]);
+
+  useEffect(() => {
+    scrollRef.current.scrollTo(0,0);
+  }, []);
+  
+
+  return (
+    <div className="flex bg-gray-50 md:flex-row flex-col h-screen transition-height duration-75 ease-out">
+      <div className="hidden md:flex h-screen flex-initial">
+        <Sidebar user={user && user} />
+      </div>
+      <div className="flex md:hidden flex-row">
+        <div className="p-2 w-full flex flex-row justify-between items-center shadow-md">
+        <HiMenu fontSize={30} className='cursor-pointer' onClick={() => setSidebar(true)} />
+        <Link to='/'>
+          <h1 className='text-4xl'>Jolie</h1>
+        </Link>
+        <Link to={`user-profile/${user?._id}`}>
+          <img src={user?.image} alt='user profile' className='w-9 h-9 rounded-full' />
+        </Link>
+        </div>
+        {sidebar && (
+          <div className="fixed w-4/5 bg-white h-screen overflow-y-auto shadow-md z-10 animate-slide-in">
+            <div className="absolute w-full flex justify-end items-center p-2">
+              <AiFillCloseCircle fontSize={30} className='cursor-pointer' onClick={() => setSidebar(false)} />
+            </div>
+            <Sidebar user={user && user} closeToggle={setSidebar} />
+          </div>
+        )}
+      </div>
+      <div className="pb-2 flex-1 h-screen overflow-y-scroll" ref={scrollRef}>
+        <Routes>
+          <Route path='/user-profile/:userId' element={<UserProfile />} />
+          <Route path='/*' element={<Pins user={user && user} />} />
+        </Routes>
+      </div>
+    </div>
+  )
+}
+
+export default Home;
