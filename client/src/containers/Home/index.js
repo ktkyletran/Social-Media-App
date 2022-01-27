d4eaf6c328efa3b787ebbac0b7bb39ad9cf06c7d
Vanity <ktkyletran@gmail.com>
2022-01-26 18:23:21 -0600

    added sidebar with navlinks and hardcoded categories

--- client/src/containers/Home/index.js
+++ client/src/containers/Home/index.js
@@ -43,7 +43,7 @@ const Home = () => {
         </Link>
         </div>
         {sidebar && (
-          <div className="fixed w-4/5 bg-white h-screen overflow-y-auto shadow-md z-10 animate-slide-in">
+          <div className="fixed w-2/5 bg-white h-screen overflow-y-auto shadow-md z-10 animate-slide-in">
             <div className="absolute w-full flex justify-end items-center p-2">
               <AiFillCloseCircle fontSize={30} className='cursor-pointer' onClick={() => setSidebar(false)} />
             </div>
