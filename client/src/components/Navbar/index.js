1a2a9408ccf753391d6c44b73aae6460c1c3f91f
Vanity <ktkyletran@gmail.com>
2022-01-27 19:20:36 -0600

    fixed vertical margin

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -8,7 +8,7 @@ const Navbar = ({ searchTerm, setSearchTerm, user }) => {
   if (!user) return null;
 
   return (
-    <div className="flex gap-2 md:gap-5 w-full mt-5">
+    <div className="flex gap-2 md:gap-5 w-full my-5">
       <div className="flex justify-start items-center w-full px-2 rounded-md bg-white border-none outline-none focus-within:shadow-small">
         <IoMdSearch fontSize={21} className='ml-1' />
         <input
