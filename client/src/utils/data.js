81af7a262637636b8d5c2db38c60709a81e08af3
Vanity <ktkyletran@gmail.com>
2022-02-08 20:02:57 -0600

    Added two new queries

--- client/src/utils/data.js
+++ client/src/utils/data.js
@@ -103,3 +103,66 @@ export const categories = [
     image: 'https://i.pinimg.com/236x/2e/63/c8/2e63c82dfd49aca8dccf9de3f57e8588.jpg',
   },
 ];
+
+export const postDetailQuery = (postId) => {
+  const query = `*[_type == "post" && _id == '${postId}']{
+    image{
+      asset->{
+        url
+      }
+    },
+    _id,
+    title, 
+    about,
+    category,
+    destination,
+    postedBy->{
+      _id,
+      userName,
+      image
+    },
+    save[]{
+      postedBy->{
+        _id,
+        userName,
+        image
+      },
+    },
+    comments[]{
+      comment,
+      _key,
+      postedBy->{
+        _id,
+        userName,
+        image
+      },
+    }
+  }`;
+  return query;
+};
+
+export const postDetailMorePostQuery = (post) => {
+  const query = `*[_type == "post" && category == '${post.category}' && _id != '${post._id}' ]{
+    image{
+      asset->{
+        url
+      }
+    },
+    _id,
+    destination,
+    postedBy->{
+      _id,
+      userName,
+      image
+    },
+    save[]{
+      _key,
+      postedBy->{
+        _id,
+        userName,
+        image
+      },
+    },
+  }`;
+  return query;
+};
