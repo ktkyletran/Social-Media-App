0d0740df3064ea383e813f297c51ae8fff204c38
Vanity <ktkyletran@gmail.com>
2022-02-10 19:33:24 -0600

    added created and saved user posts query

--- client/src/utils/data.js
+++ client/src/utils/data.js
@@ -166,3 +166,53 @@ export const postDetailMorePostQuery = (post) => {
   }`;
   return query;
 };
+
+export const userCreatedPostsQuery = (userId) => {
+  const query = `*[ _type == 'post' && userId == '${userId}'] | order(_createdAt desc){
+    image{
+      asset->{
+        url
+      }
+    },
+    _id,
+    destination,
+    postedBy->{
+      _id,
+      userName,
+      image
+    },
+    save[]{
+      postedBy->{
+        _id,
+        userName,
+        image
+      },
+    },
+  }`;
+  return query;
+};
+
+export const userSavedPostsQuery = (userId) => {
+  const query = `*[_type == 'post' && '${userId}' in save[].userId ] | order(_createdAt desc) {
+    image{
+      asset->{
+        url
+      }
+    },
+    _id,
+    destination,
+    postedBy->{
+      _id,
+      userName,
+      image
+    },
+    save[]{
+      postedBy->{
+        _id,
+        userName,
+        image
+      },
+    },
+  }`;
+  return query;
+};
